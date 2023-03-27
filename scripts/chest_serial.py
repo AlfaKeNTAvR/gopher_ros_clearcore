6c765ffa1c752e8857bb5509a1ad34065d34a4a2
Fetch Robotics <>
2023-03-27 14:40:30 -0400

    docs(chest_serial): Add docstrings

--- scripts/chest_serial.py
+++ scripts/chest_serial.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+"""
+A ROS node to communicate with the Teknic ClearCore via serial connection.
+
+Author(s):  
+    1. Yveder Joseph (ygjoseph@wpi.edu)
+    2. Nikita Boguslavskii (bognik3@gmail.com)
+"""
 
 import rospy
 import serial
@@ -10,6 +17,16 @@ from gopher_ros_clearcore.srv import SerialWrite
 
 
 class GopherSerial:
+    """Provides an interface to communicate via serial connection.
+
+    Attributes:
+        logger_pub (rospy.Publisher): A ROS publisher to publish logger data
+            received from Teknic ClearCore controller.
+        serial_write_src (rospy.Service): A ROS service to write data to Teknic 
+            ClearCore controller via serial port.
+        serialPort (serial.Serial): The serial port instance used to
+            communicate with Teknic ClearCore.
+    """
 
     def __init__(self):
         self.logger_pub = rospy.Publisher(
@@ -31,6 +48,14 @@ class GopherSerial:
         baudrate=115200,
         timeout=0.1,
     ):
+        """Sets up the serial port.
+
+        Args:
+            device_name (str): The name of the device to connect to.
+            baudrate (int): The baudrate to use for the serial connection.
+            timeout (float): The timeout for the serial connection.
+        """
+
         ports = list(serial.tools.list_ports.comports())
 
         port = ''
@@ -47,15 +72,25 @@ class GopherSerial:
         )
 
     def serial_write(self, data):
+        """Writes data to the serial port.
+
+        Args:
+            data (str): The data to be written to the serial port.
+        """
+
         self.serialPort.write(data.encode())
 
     def serial_read(self):
+        """Reads data from the serial port. 
+        
+        Publishes logger data to the logged_info topic.
+        """
+
         data = self.serialPort.readline().decode()
 
         if len(data) > 0:  # If any serial data available.
 
             data = data.split('_')
-
             command = data[0]
 
             # Publish logger data
@@ -63,6 +98,16 @@ class GopherSerial:
                 self.logger_pub.publish(data[1])
 
     def serial_write_handler(self, req):
+        """Handles serial write ROS service requests.
+
+        Args:
+            req (SerialWriteRequest): The request object containing the command 
+            to be written to the serial port.
+
+        Returns:
+            bool: A boolean value indicating whether the request was successful.
+        """
+
         self.serial_write(req.command)
 
         # TODO: Service response
@@ -81,7 +126,7 @@ def node_shutdown():
     print('Shutting down the serial connection...')
 
 
-if __name__ == '__main__':
+def main():
     # Initialize the node
     rospy.init_node('chest_serial')
     rospy.on_shutdown(node_shutdown)
@@ -92,3 +137,7 @@ if __name__ == '__main__':
 
     while not rospy.is_shutdown():
         serial.serial_read()
+
+
+if __name__ == '__main__':
+    main()
